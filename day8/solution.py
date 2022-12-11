30ddbea12194afa655a2f38d90adf3bb4548dcb6
Aygul Salahli <aygulsalahli@gmail.com>
2022-12-11 22:27:53 +0000

    day8 solution

---
+++ day8/solution.py
@@ -0,0 +1,62 @@
+a = []
+
+with open("input.txt") as f:
+    lines = f.readlines()
+    for index, line in enumerate(lines):
+        line = line.rstrip()
+        a.append([int(x) for x in line])
+
+    rows = len(a)
+    columns = len(a[0])
+    amount = 0
+    for row in range(rows):
+        for col in range(columns):
+            if row == 0 or row == len(a) - 1 or col == 0 or col == len(a[0]) - 1:
+                amount += 1
+                continue
+
+            # visible from the top?
+            visible = True
+            for i in range(row):
+                if a[i][col] >= a[row][col]:
+                    visible = False
+                    break
+
+            if visible:
+                amount += 1
+                continue
+
+            # visible from the bottom?
+            visible = True
+            for i in range(row + 1, rows):
+                if a[i][col] >= a[row][col]:
+                    visible = False
+                    break
+
+            if visible:
+                amount += 1
+                continue
+
+            # visible from the left?
+            visible = True
+            for j in range(col):
+                if a[row][j] >= a[row][col]:
+                    visible = False
+                    break
+
+            if visible:
+                amount += 1
+                continue
+
+            # visible from the right?
+            visible = True
+            for j in range(col + 1, columns):
+                if a[row][j] >= a[row][col]:
+                    # print(f"{row} {col} {a[row][col]} {j} {a[row][j]}")
+                    visible = False
+                    break
+
+            if visible:
+                amount += 1
+
+    print(amount)
