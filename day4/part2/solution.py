409be05643b20e944be76d55f1bff750d9522280
Aygul Salahli <aygulsalahli@gmail.com>
2022-12-11 12:39:52 +0000

    day 4 part 2 solution

---
+++ day4/part2/solution.py
@@ -0,0 +1,24 @@
+with open("../input.txt") as f:
+    num_of_overlaps = 0
+    lines = f.readlines()
+    for line in lines:
+        first, second = line.split(",")
+        first_lower, first_upper = first.split("-")
+        first_lower, first_upper = int(first_lower), int(first_upper)
+        second_lower, second_upper = second.split("-")
+        second_lower, second_upper = int(second_lower), int(second_upper)
+
+        if (
+            second_lower <= first_lower <= second_upper
+            or second_lower <= first_upper <= second_upper
+        ):
+            num_of_overlaps += 1
+            continue
+
+        if (
+            first_lower <= second_lower <= first_upper
+            or first_lower <= second_upper <= first_upper
+        ):
+            num_of_overlaps += 1
+
+    print(f"Number of overlaps: {num_of_overlaps}")
